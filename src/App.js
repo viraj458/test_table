f3e422cac38536a11470f7a0462a423c7e139e4c
Viraj <virajmalaka94@gmail.com>
2023-02-15 13:30:49 +0530

    Created a table using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const data = [
+  {id:1, name:"Sam", salary:"95000", age:"45", gender:"Male", dept:"Operations"},
+  {id:2, name:"Bob", salary:"80000", age:"21", gender:"Male", dept:"Support"},
+  {id:3, name:"Anne", salary:"125000", age:"25", gender:"Female", dept:"Analytics"},
+  {id:4, name:"Julia", salary:"73000", age:"30", gender:"Female", dept:"Analytics"},
+  {id:5, name:"Matt", salary:"159000", age:"33", gender:"Male", dept:"Sales"},
+  {id:6, name:"Jeff", salary:"112000", age:"27", gender:"Male", dept:"Operations"},
+]
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr style={{backgroundColor: "#FAFAFA"}}>
+          <th>e_id</th>
+          <th>e_name</th>
+          <th>e_salary</th>
+          <th>e_age</th>
+          <th>e_gender</th>
+          <th>e_dept</th>
+        </tr>
+        {data.map((val, key)=>{
+          return(
+            <tr key={key}>
+              <td style={{textAlign:"end"}}>{val.id}</td>
+              <td>{val.name}</td>
+              <td style={{textAlign:"end"}}>{val.salary}</td>
+              <td style={{textAlign:"end"}}>{val.age}</td>
+              <td>{val.gender}</td>
+              <td>{val.dept}</td>
+            </tr>
+          )
+          
+        })}
+
+      </table>
     </div>
   );
 }
